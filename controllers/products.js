75b2f9a10a536c379f1a1c9e0da39f2b1ccb56ee
Jesus Matute <mttrox@gmail.com>
2022-08-17 23:00:58 -0400

    part of product controller

---
+++ controllers/products.js
@@ -0,0 +1,117 @@
+const formidable = require("formidable");
+const _ = require("lodash");
+const fs = require("fs");
+const Product = require("../models/product");
+const { errorHandler } = require("../helpers/dbErrorHandler");
+
+//Middleware rest
+
+exports.productById = (req, res, next, id) => {
+  Product.findById(id)
+    .populate("category")
+    .exec((error, product) => {
+      if (error || !product) {
+        return res.status(400).json({
+          error: "Product not found",
+        });
+      }
+      req.product = product;
+      next();
+    });
+};
+
+exports.read = (req, res) => {
+  req.product.photo = undefined;
+  return res.json(req.product);
+};
+
+exports.create = (req, res) => {
+  let form = new formidable.IncomingForm();
+  form.keepExtensions = true;
+  form.parse(req, (error, fields, files) => {
+    if (error) {
+      return res.status(400).json({
+        error: "Image could not be uploded",
+      });
+    }
+    //Check for all fields
+    const { name, description, price, category, quantity, shipping } = fields;
+
+    if (
+      !name ||
+      !description ||
+      !price ||
+      !category ||
+      !quantity ||
+      !shipping
+    ) {
+      return res.status(400).json({
+        error: "All fields are required",
+      });
+    }
+    let product = new Product(fields);
+    if (files.photo) {
+      if (files.photo.size > 1000000) {
+        return res.status(400).json({
+          error: "Image max size is 1mb",
+        });
+      }
+      product.photo.data = fs.readFileSync(files.photo.path);
+      product.photo.contentType = files.photo.type;
+    }
+    product.save((error, result) => {
+      if (error) {
+        return res.status(400).json({
+          error: errorHandler(error),
+        });
+      }
+      res.json(result);
+    });
+  });
+};
+
+exports.remove = (req, res) => {
+  let product = req.product;
+  product.remove((error, _deletedProduct_) => {
+    if (error) {
+      return res.status(400).json({
+        error: errorHandler(error),
+      });
+    }
+    res.json({
+      message: "Product deleted successfully",
+    });
+  });
+};
+
+exports.update = (req, res) => {
+  let form = new formidable.IncomingForm();
+  form.keepExtensions = true;
+  form.parse(req, (error, fields, files) => {
+    if (error) {
+      return res.status(400).json({
+        error: errorHandler(error),
+      });
+    }
+    let product = req.product;
+    product = _.extend(product, fields);
+
+    if (fields.photo) {
+      if (files.photo.size > 1000000) {
+        return res.status(400).json({
+          error: "Image max size is 1mb",
+        });
+      }
+      product.photo.data = fs.readFileSync(files.photo.path);
+      product.photo.contentType = files.photo.type;
+    }
+    product.save((error, result) => {
+      if (error) {
+        return res.status(400).json({
+          error: errorHandler(error),
+        });
+      }
+      res.json(result);
+    });
+  });
+};
